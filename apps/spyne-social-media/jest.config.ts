d747dbec1a2daf45c490ec485fe05368425ac031
Akash Chand <akashchand1703@gmail.com>
2024-06-06 13:20:25 +0530

    Initial commit

---
+++ apps/spyne-social-media/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'spyne-social-media',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/spyne-social-media',
+};
