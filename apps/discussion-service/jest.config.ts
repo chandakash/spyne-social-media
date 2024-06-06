9a29c87db1e82fd5f3f1a0ab70c972ce8f0afe33
Akash Chand <akashchand1703@gmail.com>
2024-06-06 13:48:27 +0530

    init: monorepo setup with two services user & discussion, using nx

--- apps/discussion-service/jest.config.ts
+++ apps/discussion-service/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'spyne-social-media',
+  displayName: 'discussion-service',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/spyne-social-media',
+  coverageDirectory: '../../coverage/apps/discussion-service',
 };
